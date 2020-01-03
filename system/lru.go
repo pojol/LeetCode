1fe3d70b2ff5bb2dd4a5703d926278586deb1048
pojol <pojol@outlook.com>
2020-01-03 15:43:38 +0800

    基于map 和 list 实现的lru 。

---
+++ system/lru.go
@@ -0,0 +1,73 @@
+package system
+
+import (
+	"container/list"
+)
+
+// https://leetcode.com/problems/lru-cache/
+
+// LRUCache lru结构
+type LRUCache struct {
+	cap      int
+	mapCache map[int]*list.Element
+	list     *list.List
+}
+
+// LRUNode 存放数据
+type LRUNode struct {
+	key int
+	val int
+}
+
+// Constructor 构造
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		cap:      capacity,
+		mapCache: make(map[int]*list.Element),
+		list:     list.New(),
+	}
+}
+
+// Get 获取数据
+func (lp *LRUCache) Get(key int) int {
+
+	if elem, ok := lp.mapCache[key]; ok {
+		lp.list.MoveToFront(elem)
+		return lp.mapCache[key].Value.(LRUNode).val
+	}
+
+	return -1
+}
+
+// Put 存放数据
+func (lp *LRUCache) Put(key int, value int) {
+
+	if elem, ok := lp.mapCache[key]; ok {
+		lp.list.MoveToFront(elem)
+		elem.Value = LRUNode{
+			key: key,
+			val: value,
+		}
+	} else {
+
+		if lp.list.Len() >= lp.cap { // 淘汰尾部元素
+			delete(lp.mapCache, lp.list.Back().Value.(LRUNode).key)
+			lp.list.Remove(lp.list.Back())
+		}
+
+		nod := LRUNode{
+			key: key,
+			val: value,
+		}
+		lp.list.PushFront(nod)
+		lp.mapCache[nod.key] = lp.list.Front()
+	}
+
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
---
+++ system/lru_test.go
@@ -0,0 +1,18 @@
+package system
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLru(t *testing.T) {
+
+	lru := Constructor(2)
+	lru.Put(2, 1)
+	lru.Put(2, 2)
+	assert.Equal(t, lru.Get(2), 2)
+	lru.Put(1, 1) // evicts key 2
+	lru.Put(4, 1)
+	assert.Equal(t, lru.Get(2), -1)
+}
